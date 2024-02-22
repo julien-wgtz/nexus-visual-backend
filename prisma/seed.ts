3a7591e9e5d8f508bb4463fe4b68ea2559532bf4
julien-wagentrutz <julien.wagentrutz@hetic.net>
2024-02-22 15:25:31 +0100

    :seedling: add seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  const userAdmin = await prisma.user.upsert({
+    where: { email: 'julien.wgtz@outlook.com' },
+    update: {},
+    create: {
+      email: 'julien.wgtz@outlook.com',
+      password: '$2b$10$UjAqzANIwkVBPHUjyzyC8OU6ashGJVl7t5687J4xtiF9Xh0K9VALe',
+      confirmed: true,
+      role: 'ADMIN',
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

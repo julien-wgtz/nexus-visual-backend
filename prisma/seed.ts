c2b7fe0492e0e74fc92f9df858601296d9e6b6e4
julien-wagentrutz <julien.wagentrutz@hetic.net>
2024-02-27 09:08:39 +0000

    :card_file_box: Add notion token to user data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,7 @@ async function main() {
       password: '$2b$10$UjAqzANIwkVBPHUjyzyC8OU6ashGJVl7t5687J4xtiF9Xh0K9VALe',
       confirmed: true,
       role: 'ADMIN',
+      notionToken: 'secret_3fhv1cScjcPwOquKJsCPpQHMOzLKJZVcD4pqVyI2tNP',
     },
   });
 }
